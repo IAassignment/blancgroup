fc3acf648d60ea530c32206fda88226e2416a3c1
liyi98 <ly19980804@gmail.com>
2017-03-27 20:28:00 +0800

    quantity javascript

---
+++ js/quantity.js
@@ -0,0 +1,21 @@
+funtion quantityCtrl(x) {
+	var y = document.getElementById("qty").innerHTML;
+	if (x === 0) {
+		if (y <= 1) {
+			// do nothing
+		} else {
+			y--;
+			document.getElementById("qty").innerHTML = y;
+			var x = parseInt(document.getElementById("itemPrice")).innerHTML;
+			document.getElementById("displayPrice").innerHTML = (x * y).toLocaleString();
+		}
+	}else if (x === 1){
+		if (y < 20){
+			y++;
+			document.getElementById("qty").innerHTML = y;
+			var x = parseInt(document.getElementById("itemPrice")).innerHTML;
+			document.getElementById("displayPrice").innerHTML = (x*y).toLocaleString();
+		}
+	}
+
+}
